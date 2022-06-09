7dcf7ad3b1de4b2268cd19db9c362240a28214fc
Tyler Mitchell <mitchellctyler@gmail.com>
2022-06-09 16:15:51 -0700

    update commit

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -23,14 +23,16 @@ def hello(banana, num):
     return render_template("hello.html", banana=banana, num=num)
 
 
-# for a route '/users/____/____', two parameters in the url get passed as username and id
-@app.route('/users/<username>/<id>')
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
-
-# app.run(debug=True) should be the very last statement!
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template("lists.html", random_numbers=[3, 1, 5], students=student_info)
 
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
